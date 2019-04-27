2018034526df99490c638f35ac9b7a92700dc78a
shamaz saeed <shamazsaeed332@gmail.com>
2019-04-27 06:09:47 -0700

    deploting

---
+++ precache-manifest.a754c6abcf2a113d60d7844a5d7d7c76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "06d817c1325911f590ff",
+    "url": "/static/js/main.76f51e91.chunk.js"
+  },
+  {
+    "revision": "f0ce8d1e736689b4ba6f",
+    "url": "/static/js/2.ddabe83d.chunk.js"
+  },
+  {
+    "revision": "06d817c1325911f590ff",
+    "url": "/static/css/main.f5ca8e1e.chunk.css"
+  },
+  {
+    "revision": "f0ce8d1e736689b4ba6f",
+    "url": "/static/css/2.7e5952bf.chunk.css"
+  },
+  {
+    "revision": "e96fa5ec2a689da68a6d411ec372b43d",
+    "url": "/index.html"
+  }
+];
